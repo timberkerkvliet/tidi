d9aa19c9cefbd9e0974dfc85384be466d252666b
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-08-07 17:04:11 +0200

    ensure root scope

--- test_tidi/test_context.py
+++ test_tidi/test_context.py
@@ -2,66 +2,64 @@ from unittest import TestCase
 
 from test_tidi import context_composition
 from test_tidi.context_composition import StringGenerator, HelloGenerator, TimberGenerator, App
-from tidipy import scan, get_resolver, reset, add_context, ensure_scope
+from tidipy import scan, get_resolver, reset, ensure_scope
 
 
 class TestContext(TestCase):
     def setUp(self) -> None:
         scan(context_composition)
-        self.resolver = get_resolver()
 
     def tearDown(self) -> None:
         reset()
 
     def test_production(self):
-        add_context(environment='prod')
-        result = self.resolver(StringGenerator)
+        ensure_scope(scope_id='root', context={'environment': 'prod'})
+        result = get_resolver()(StringGenerator)
 
         self.assertEqual(result.generate(), 'Timber')
 
     def test_specific_type(self):
-        add_context(environment='test')
-        result = self.resolver(HelloGenerator)
+        ensure_scope(scope_id='root', context={'environment': 'test'})
+        result = get_resolver()(HelloGenerator)
 
         self.assertEqual(result.generate(), 'Hello')
 
     def test_resolves_with_additional_values(self):
-        add_context(environment='test', some_extra_key='iets')
-        result = self.resolver(HelloGenerator)
+        ensure_scope(scope_id='root', context={'environment': 'test', 'extra_key': 'iets'})
+        result = get_resolver()(HelloGenerator)
 
         self.assertEqual(result.generate(), 'Hello')
 
     def test_cannot_resolve_with_multiple_candidates(self):
         with self.assertRaises(Exception):
-            self.resolver(StringGenerator)
+            get_resolver()(StringGenerator)
 
     def test_can_resolve_specific(self):
-        result = self.resolver(TimberGenerator)
+        result = get_resolver()(TimberGenerator)
 
         self.assertEqual(result.generate(), 'Timber')
 
     def test_resolve_app(self):
-        add_context(environment='test')
-        app = self.resolver(App)
+        ensure_scope(scope_id='root', context={'environment': 'test'})
+        app = get_resolver()(App)
 
         self.assertEqual(app.generate(), 'Hello')
 
     def test_cannot_change_context(self):
-        add_context(environment='test')
+        ensure_scope(scope_id='root', context={'environment': 'test'})
 
         with self.assertRaises(Exception):
-            add_context(environment='prod')
+            ensure_scope(scope_id='root', context={'environment': 'prod'})
 
     def test_cannot_change_context_in_child(self):
-        add_context(environment='test')
-        ensure_scope('child', scope_type='child')
+        ensure_scope(scope_id='root', context={'environment': 'test'})
 
         with self.assertRaises(Exception):
-            add_context('child', environment='prod')
+            ensure_scope('child', scope_type='child', context={'environment': 'prod'})
 
     def test_adding_context_to_child_scope_does_not_influence_root(self):
-        ensure_scope(scope_id='child', scope_type='child')
-        add_context(scope_id='child', environment='test')
+        ensure_scope(scope_id='child', scope_type='child', context={'environment': 'test'})
+
         root_resolver = get_resolver()
         child_resolver = get_resolver('child')
 
--- tidipy/scope_manager_api.py
+++ tidipy/scope_manager_api.py
@@ -22,10 +22,6 @@ def ensure_scope(
     )
 
 
-def add_context(scope_id: str = 'root', **kwargs) -> None:
-    scope_manager.add_context(scope_id, kwargs)
-
-
 def get_resolver(scope_id: str = 'root') -> Resolver:
     return scope_manager.get_resolver(scope_id)
 
