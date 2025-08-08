17b4d0007d12afb908a39ae4ee99df1ce37b0a8c
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-08-08 12:46:40 +0200

    clean up manager code

--- tidipy/scope_manager.py
+++ tidipy/scope_manager.py
@@ -1,16 +1,10 @@
-from typing import Optional
-
 from .dependency import Composer
 from .scope import Scope
-from .scope_context import ScopeContext
-from .scope_type import ScopeType, RootType
-from .resolver import Resolver
 
 
 class ScopeManager:
     def __init__(self):
         self._composers: set[Composer] = set()
-        self._scopes: dict[str, Scope] = {}
 
     def add_composer(self, composer: Composer) -> None:
         if composer in self._composers:
@@ -23,60 +17,6 @@ class ScopeManager:
 
     def get_composers(self) -> set[Composer]:
         return self._composers
-
-    def get_resolver(self, scope_id: str) -> Resolver:
-        return self._get_scope(scope_id).resolver()
-
-    def _get_scope(self, scope_id: str) -> Scope:
-        if scope_id == 'root':
-            self.ensure_scope(scope_id='root', scope_type=RootType())
-
-        return self._scopes[scope_id]
-
-    def ensure_scope(
-        self,
-        scope_id: str,
-        scope_type: ScopeType,
-        parent_id: Optional[str] = None,
-        context: Optional[ScopeContext] = None
-    ) -> None:
-        scope = self._create_scope(scope_id, scope_type, parent_id, context or ScopeContext.empty())
-        if scope_id not in self._scopes:
-            self._scopes[scope_id] = scope
-            return
-
-        existing_scope = self._scopes[scope_id]
-        if existing_scope.get_type() != scope_type:
-            raise Exception
-        if existing_scope.get_parent() is None and parent_id is not None:
-            raise Exception
-        if existing_scope.get_parent() is not None and existing_scope.get_parent().get_id() != parent_id:
-            raise Exception
-        if context is not None and existing_scope.get_context() != context:
-            raise Exception
-
-    def _create_scope(
-        self,
-        scope_id: str,
-        scope_type: ScopeType,
-        parent_id: Optional[str],
-        context: ScopeContext
-    ) -> Scope:
-        return Scope(
-            scope_id=scope_id,
-            parent=self._get_scope(parent_id) if parent_id is not None else None,
-            scope_type=scope_type,
-            composers=self._composers,
-            context=context
-        )
-
-    def clear_scope(self, scope_id: str) -> None:
-        if scope_id not in self._scopes:
-            return
-        deleted_scope = self._scopes.pop(scope_id)
-        child_ids = {scope.get_id() for scope in self._scopes.values() if scope.get_parent() == deleted_scope}
-        for child_id in child_ids:
-            self.clear_scope(child_id)
 
     def reset(self) -> None:
         self.__init__()
