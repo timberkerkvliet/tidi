a825d6e55d8497413dc475e115c10caa3a863b62
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-08-08 12:51:22 +0200

    rename api

--- tidipy/__init__.py
+++ tidipy/__init__.py
@@ -1,7 +1,5 @@
 from .composer import composer
 from .resolver import Resolver
 from .scan import scan
-from .scope_manager_api import ensure_scope, clear_scope, reset, get_resolver
+from .scope_api import ensure_scope, clear_scope, reset, get_resolver
 from .auto_compose import auto_compose
-
-
