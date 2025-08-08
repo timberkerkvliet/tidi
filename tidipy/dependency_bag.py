9dcb4070724d719e3a895e0bf3c9b8d702b85d4b
Timber Kerkvliet <timberkerkvliet@gmail.com>
2025-08-08 15:26:23 +0200

    small refactor dependency bag

--- tidipy/dependency_bag.py
+++ tidipy/dependency_bag.py
@@ -15,13 +15,22 @@ class DependencyBag:
     def from_dependencies(dependencies: Iterable[Dependency]) -> DependencyBag:
         return DependencyBag({dependency.get_id(): dependency for dependency in dependencies})
 
-    def _get_from_dependency(self, dependency: Dependency, resolver: Resolver):
-        concrete = dependency.make_concrete(resolver)
+    def _get_candidates(
+        self,
+        dependency_type: Type,
+        dependency_id: Optional[str],
+        scope_context: ScopeContext
+    ) -> list[Dependency]:
+        candidates = [
+            dependency
+            for dependency in self._dependencies.values()
+            if issubclass(dependency.get_dependency_type(), dependency_type)
+               and dependency.get_conditions().is_fulfilled_by(scope_context.values())
+        ]
+        if dependency_id is not None:
+            candidates = [dependency for dependency in candidates if dependency.get_id() == dependency_id]
 
-        if dependency.supports_storing():
-            self._dependencies[dependency.get_id()] = concrete
-
-        return concrete.value
+        return candidates
 
     def find(
         self,
@@ -30,18 +39,17 @@ class DependencyBag:
         resolver: Resolver,
         scope_context: ScopeContext
     ) -> Optional[Dependency]:
-        candidates = [
-            dependency
-            for dependency in self._dependencies.values()
-            if issubclass(dependency.get_dependency_type(), dependency_type)
-            and dependency.get_conditions().is_fulfilled_by(scope_context.values())
-        ]
-        if dependency_id is not None:
-            candidates = [dependency for dependency in candidates if dependency.get_id() == dependency_id]
-        if len(candidates) == 1:
-            dependency = next(iter(candidates))
-            return self._get_from_dependency(dependency, resolver)
+        candidates = self._get_candidates(dependency_type, dependency_id, scope_context)
+
+        if len(candidates) == 0:
+            return None
         if len(candidates) > 1:
             raise Exception(f'More than 1 candidate for type {dependency_type}')
 
-        return None
+        dependency = next(iter(candidates))
+        concrete = dependency.make_concrete(resolver)
+
+        if dependency.supports_storing():
+            self._dependencies[dependency.get_id()] = concrete
+
+        return concrete.value
